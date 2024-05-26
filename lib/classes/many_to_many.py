dd3d1da787fbfbd9acd52959780942971fdd61f5
kenkipkoec <itsmetrokenaki@gmal.com>
2024-05-26 17:05:57 +0300

    first commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,85 @@
 class Article:
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
+        self._title = title
+        self._author = author
+        self._magazine = magazine
+
+        self._magazine.add_article(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
 
     def articles(self):
-        pass
-
-    def magazines(self):
-        pass
+        return self._articles
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
+
+    def magazines(self):
+        return list(set(article.magazine for article in self._articles))
 
     def topic_areas(self):
-        pass
+        return list(set(article.magazine.category for article in self._articles))
+
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name
+        self._category = category
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        self._category = value
+
+    def add_article(self, article):
+        self._articles.append(article)
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self._articles))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        pass
+        authors = []
+        for article in self._articles:
+            authors.extend(article.author.articles())
+        author_counts = {author: authors.count(author) for author in authors}
+        return [author for author, count in author_counts.items() if count > 2]
--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
+import pytest
+from classes.many_to_many import Article
 
-def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+
+
+@pytest.fixture(autouse=True)
+def clear_articles():
+    Article.all = []
